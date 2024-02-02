5c5aa1dbc946605a1c7af64af1e9fcef9e5e7a60
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-02-02 22:05:46 +0900

    1

--- srcs/requirements/mariadb/tools/setup-mariadb.sh
+++ srcs/requirements/mariadb/tools/setup-mariadb.sh
@@ -1,3 +1,21 @@
-#! /bin/bash
+#!/bin/bash
 
-service mysql start
+# Set root password using ALTER USER
+mysql -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+# Configure MariaDB
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<EOF
+DELETE FROM mysql.user WHERE User ='${MYSQL_USER}';
+DELETE FROM mysql.db WHERE User ='${MYSQL_USER}';
+FLUSH PRIVILEGES;
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+# Start MariaDB
+exec mysqld --user=root
