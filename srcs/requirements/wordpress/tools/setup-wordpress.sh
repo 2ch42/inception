ee844feb6757ba43f7f78fa93e8382a0dee5aab7
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-02-03 22:57:55 +0900

    1

--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-wget https://wordpress.org/latest.tar.gz
-tar -xvf latest.tar.gz
-mv /wordpress/* /var/www/html/wordpress
-sleep 3
+mv -f /wordpress/* /var/www/html/wordpress
 chown -R www-data:www-data /var/www/html/wordpress
-sleep 3
-rm -r /wordpress latest.tar.gz
-mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 
-sed -i "s/define('DB_NAME', '.*');/define('DB_NAME', '$MYSQL_DATABASE');/" "/var/www/html/wordpress/wp-config.php"
-sed -i "s/define('DB_USER', '.*');/define('DB_USER', '$MYSQL_USER');/" "/var/www/html/wordpress/wp-config.php"
-sed -i "s/define('DB_PASSWORD', '.*');/define('DB_PASSWORD', '$MYSQL_PASSWORD');/" "/var/www/html/wordpress/wp-config.php"
-
-mv /usr/local/bin/my.cnf /etc/php/7.4/fpm/
+#sed -i "s/define('DB_NAME', '.*');/define('DB_NAME', '$MYSQL_DATABASE');/" "/var/www/html/wordpress/wp-config.php"
+#sed -i "s/define('DB_USER', '.*');/define('DB_USER', '$MYSQL_USER');/" "/var/www/html/wordpress/wp-config.php"
+#sed -i "s/define('DB_PASSWORD', '.*');/define('DB_PASSWORD', '$MYSQL_PASSWORD');/" "/var/www/html/wordpress/wp-config.php"
+sed -i 's|^listen = .*|listen = 0.0.0.0:9000|' /etc/php/7.4/fpm/pool.d/www.conf
 service php7.4-fpm start
