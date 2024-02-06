7dfe9ee7a739301fdc79ff60032f3c7567a5ebfe
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-02-06 20:43:39 +0900

    1

--- srcs/requirements/nginx/tools/setup-nginx.sh
+++ srcs/requirements/nginx/tools/setup-nginx.sh
@@ -2,6 +2,8 @@
 
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=learner/CN=changhyl" -keyout /etc/ssl/private/changhyl_p.key -out /etc/ssl/certs/changhyl.crt
 chmod 600 /etc/ssl/certs/changhyl.crt /etc/ssl/private/changhyl_p.key
-service nginx start
+
+service nginx reload
+service nginx restart
 
 tail -f
--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -10,10 +10,13 @@ cd /var/www/html/wordpress
 wp config set DB_NAME "'$MYSQL_DATABASE'" --raw --allow-root
 wp config set DB_USER "'$MYSQL_USER'" --raw --allow-root
 wp config set DB_PASSWORD "'$MYSQL_PASSWORD'" --raw --allow-root
-wp config set DB_HOST "'$MYSQL_HOST'" --raw --allow-root
+wp config set DB_HOST "'$MYSQL_DATABASE'" --raw --allow-root
 wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 wp user create "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD" --allow-root
 
 sed -i 's|^listen = .*|listen = 0.0.0.0:9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
 service php7.4-fpm start
+
+
+tail -f
